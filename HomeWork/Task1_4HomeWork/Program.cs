176e30d67dfaa7fa8aac43e2fdea15d1c55fcd0b
«Ulyana <eksterua@gmail.com>
2023-03-06 22:26:20 +0300

    HW_Task1_4

---
+++ HomeWork/Task1_4HomeWork/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+Console.WriteLine("Введите число");
+string s_a = Console.ReadLine()!;
+int a = int.Parse(s_a);
+int b = 2;
+while (b <= a) {
+    Console.Write(b + " ");
+    b = b + 2;
+}
