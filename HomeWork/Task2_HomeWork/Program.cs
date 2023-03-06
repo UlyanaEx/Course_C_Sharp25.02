df9da42280d379b526881581d2205c3f30e014c7
«Ulyana <eksterua@gmail.com>
2023-03-06 22:09:45 +0300

    HW_Task1_2

---
+++ HomeWork/Task2_HomeWork/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих
+
+Console.WriteLine("Введите первое число");
+string s_a = Console.ReadLine()!;
+Console.WriteLine("Отлично! Введите второе число");
+string s_b = Console.ReadLine()!;
+Console.WriteLine("Супер! А теперь введите третье число");
+string s_c = Console.ReadLine()!;
+int a = int.Parse(s_a);
+int b = int.Parse(s_b);
+int c = int.Parse(s_c);
+if (a>=b)
+{
+    if (a>=c)
+    {
+        Console.WriteLine("max = " + a);
+    }
+    else {
+        Console.WriteLine("max = " + c);
+    }
+}
+else if (b>=c) {
+    Console.WriteLine("max = " + b);
+}
+else {
+    Console.WriteLine("max = " + c);
+}
