f149f26b491e29b0c04ca7d08e41e1540963fbc0
«Ulyana <eksterua@gmail.com>
2023-03-12 10:17:46 +0300

    Task 2_Practice 4

---
+++ Practice4/4_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая
+//принимает на вход число N и выдаёт
+//произведение чисел от 1 до N
+
+int MultiplyNum(int num)
+{
+    int all_mult =1;
+    for (int i = 1; i <= num; i++)  
+        all_mult = all_mult * i;
+    return all_mult;
+    
+}
+
+Console.WriteLine("ВВедите число");
+int A = int.Parse(Console.ReadLine()!);
+int result = MultiplyNum(A);
+Console.WriteLine(result);
