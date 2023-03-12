a11593c62f69cf85d62157fd72fb8d02c9019f5e
«Ulyana <eksterua@gmail.com>
2023-03-12 12:40:28 +0300

    Task 2_HomeWork4

---
+++ Practice4/4_3/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая выводит
+//массив из 8 элементов, заполненный нулями и
+//единицами в случайном порядке.
+//[1,0,1,1,0,1,0,0]
+
+//int[] Array = new int[8];
+//
+//void FillArray (int[] collection)
+//{
+//    int length = collection.Length;
+//    int index = 0;
+//    while(index<length){
+//        collection[index] = new Random().Next(0,2);
+//        index++;
+//    }
+//}
+//void PrintArray (int[]col)
+//{
+//    int count = col.Length;
+//    int position = 0;
+//    while (position < count)
+//    {
+//        Console.WriteLine(col[position]);
+//        position++;
+//    }
+//}
+//FillArray(Array);
+//PrintArray(Array);
+
+void Massiv (int length)
+{
+    int[] Array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        Array[i] = new Random().Next(2);
+        Console.WriteLine(Array[i]);
+    }
+}
+Massiv(int.Parse(Console.ReadLine()!));
---
+++ Practice4/HomeWork4/Task2_HomeWork4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на
+//вход число и выдаёт сумму цифр в числе
+
+int DigitSum (int num)
+{
+    int a = 0;
+    for (int i = 0; num > 0; i++)
+    {
+        a  = a + num % 10;
+        num = num / 10;
+    }
+   return a;
+}
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine()!);
+int result = DigitSum(a);
+Console.WriteLine(result);
