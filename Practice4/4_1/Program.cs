06522c5ac6ddf9e67a7be4beb5b5d961d592041c
«Ulyana <eksterua@gmail.com>
2023-03-12 10:09:04 +0300

    Task 1_Practice 4

---
+++ Practice4/4_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine(9/10);
+
+int FigureSum (int num)
+{
+    int s = 0;
+    for (int i = 1; num > 0; i++)
+    {
+        num = num / 10;
+        s = i;
+    }
+    return s;
+}
+
+
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine()!);
+int result = FigureSum (a);
+Console.WriteLine(result);
