68e2fb8156d76fa4c8f962e32d6c750566f4236e
«Ulyana <eksterua@gmail.com>
2023-03-19 16:51:28 +0300

    Task 1_HW6

---
+++ Practice6/HomeWork6/Task1_HomeWork6/Program.cs
@@ -0,0 +1,22 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь
+
+
+int CountPositive(int num)
+{
+    Console.WriteLine("Введите числа");
+    int a = 0;
+    for (int i = 0; i < num; i++)
+    {
+        int n = int.Parse(Console.ReadLine()!);
+        if (n > 0)
+            a++;
+    }
+    return a;
+}
+
+Console.WriteLine("Сколько чисел будет введено?");
+int n = int.Parse(Console.ReadLine()!);
+
+int result = CountPositive(n);
+Console.WriteLine($"Количество положительных чисел - {result}");
