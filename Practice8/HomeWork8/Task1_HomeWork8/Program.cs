12594ad356f2b8228cfd87bb8fa7c886ed34fafc
«Ulyana <eksterua@gmail.com>
2023-03-24 10:52:33 +0300

    Task1_HomeWork8

---
+++ Practice8/HomeWork8/Task1_HomeWork8/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте двумерный массив. Напишите программу, которая
+// упорядочит по убыванию элементы каждой строки двумерного массива
+
+void Print(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    return arr;
+}
+
+int[,] Sort_max_to_min(int[,] array)
+{
+    int row = array.GetLength(0);
+    int column = array.GetLength(1);
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            for (int f = j + 1; f < column; f++)
+            {
+                if (array[i, j] < array[i, f])
+                {
+                    (array[i, f], array[i, j]) = (array[i, j], array[i, f]);
+                }
+            }
+    return array;
+}
+
+Console.WriteLine("Введите число строк в массиве");
+int num_row = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число столбцов в массиве");
+int num_column = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите диапозон значений элементов массива - min и max");
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(num_row, num_column, start, stop);
+Print(mass);
+Print(Sort_max_to_min(mass));
