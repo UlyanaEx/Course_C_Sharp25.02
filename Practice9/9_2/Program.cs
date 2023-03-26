c974572ca0d245915a586cc2c25cf08458cc6990
«Ulyana <eksterua@gmail.com>
2023-03-26 19:37:10 +0300

    9_2

---
+++ Practice9/9_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// : Задайте значения M и N. Напишите программу, которая
+// рекурсивно выведет все натуральные числа в промежутке от M до N
+
+void Output (int M, int N)
+{
+if (N < M) return;
+Output (M, N-1);
+Console.Write ($"{N} ");
+}
+
+Console.WriteLine("Введите число - нижнюю границу");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число - верхнюю границу");
+int num2 = int.Parse(Console.ReadLine()!);
+Output(num1, num2);
