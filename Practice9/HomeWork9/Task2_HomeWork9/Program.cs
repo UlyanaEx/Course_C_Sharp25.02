0880f67b08930ffe5bf50ac3d25c4b1f2c81677f
«Ulyana <eksterua@gmail.com>
2023-03-26 20:49:59 +0300

    Task2_HW9

---
+++ Practice9/HomeWork9/Task2_HomeWork9/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N с помощью рекурсии
+
+int Sum(int M, int N)
+{
+    if (N < M) return 0;
+    return Sum(M, N - 1) + N;
+}
+
+Console.WriteLine("Введите число - нижнюю границу");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число - верхнюю границу");
+int num2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Sum(num1, num2));
