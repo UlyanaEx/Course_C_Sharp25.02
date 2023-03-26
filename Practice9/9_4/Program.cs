e8986f9e70506cf9450f03f2539f26aa4edaeb8b
«Ulyana <eksterua@gmail.com>
2023-03-26 20:07:32 +0300

    9_4

---
+++ Practice9/9_4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+
+int Degree (int A, int B)
+{
+if (B == 0) return 1;
+return Degree (A, B-1) * A;
+}
+
+Console.WriteLine("Введите число - основание степени");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число - степень");
+int num2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Degree (num1, num2));
