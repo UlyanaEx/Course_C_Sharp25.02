486cdc4e0f38899450de724817d46f9da779c957
«Ulyana <eksterua@gmail.com>
2023-03-26 19:56:07 +0300

    9_3

---
+++ Practice9/9_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр. Использовать рекурсию.
+
+int Sum_figures (int N)
+{
+   if (N == 0) return 0;
+ return Sum_figures (N/10) + N % 10;
+}
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Sum_figures (num));
