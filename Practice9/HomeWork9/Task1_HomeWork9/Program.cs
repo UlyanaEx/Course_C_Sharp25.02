3c283be992ecebc2802873785a58770697dd025b
«Ulyana <eksterua@gmail.com>
2023-03-26 20:42:32 +0300

    Task1_HW9

---
+++ Practice9/HomeWork9/Task1_HomeWork9/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все
+// чётные натуральные числа в промежутке от M до N с помощью рекурсии
+
+void Output(int M, int N)
+{
+    if (N % 2 != 0)
+        N = N - 1;
+
+    if (N < M) return;
+    Output(M, N - 2);
+    Console.Write($"{N} ");
+}
+
+Console.WriteLine("Введите число - нижнюю границу");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число - верхнюю границу");
+int num2 = int.Parse(Console.ReadLine()!);
+Output(num1, num2);
